ef2e85d94f2785255fd80921793c3b188d1ff163
dstegner <dmstegner@gmail.com>
2020-08-22 15:40:18 -0400

    1. Implemented Api client and added setting to config

--- AngularJeopardy/Api/Interface/IJeopardyClient.cs
+++ AngularJeopardy/Api/Interface/IJeopardyClient.cs
@@ -1,7 +1,24 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AngularJeopardy.Models;
+
 namespace AngularJeopardy.Api.Interface
 {
     public interface IJeopardyClient
     {
-        
+        /// <summary>
+        /// Gets a list of categories from the api endpoint
+        /// </summary>
+        /// <param name="count">How many <see cref="Category"/> to  get</param>
+        /// <param name="offset">Starting point in list to get <see cref="Category"/></param>
+        /// <returns>A List of <see cref="Category"/></returns>
+        Task<List<Category>> GetCategoriesAsync(int count = 5, int offset = 0);
+
+        /// <summary>
+        /// Gets a list of questions from the api endpoint based on a category
+        /// </summary>
+        /// <param name="categoryId">Id of the <see cref="Category"/></param>
+        /// <returns>A list of <see cref="Question"/></returns>
+        Task<List<Question>> GetQuestionByCategoryAsync(int categoryId);
     }
 }
