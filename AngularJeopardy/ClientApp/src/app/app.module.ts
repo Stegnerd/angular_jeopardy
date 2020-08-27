9a7bcfa2b832c72e7bc88cc72050d53c9a118e08
Dalton Stegner <dmstegner@gmail.com>
2020-08-26 21:05:59 -0400

    1. Added category select component and app

--- AngularJeopardy/ClientApp/src/app/app.module.ts
+++ AngularJeopardy/ClientApp/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './components/home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { CategoryselectComponent } from './components/categoryselect/categoryselect.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    CategoryselectComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
---
+++ AngularJeopardy/ClientApp/src/app/components/categoryselect/categoryselect.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-categoryselect',
+  templateUrl: './categoryselect.component.html',
+  styleUrls: ['./categoryselect.component.css']
+})
+export class CategoryselectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
